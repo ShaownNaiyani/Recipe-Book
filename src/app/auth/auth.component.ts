d6b581a69908718385dbdd285d98251d4b71f8eb
shaown224 <60014974+shaown224@users.noreply.github.com>
2023-08-12 23:14:22 +0600

    Authentication,AutoLogin,AutoLogout and Authgurd are added successfully

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,61 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthResponse, AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+import { Router } from "@angular/router";
+
+
+@Component({
+    selector:'app-auth',
+    templateUrl:'./auth.component.html'
+    
+})
+export class AuthComponent{
+
+    constructor(private authService: AuthService,private router:Router){}
+
+    isLoginMode = true;
+    isLoadingMode = false;
+    isRegistered = false;
+    error:string = null;
+
+    onSwitchMode(){
+        this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form:NgForm){
+
+        let authObs: Observable<AuthResponse>
+
+        if(form.valid){
+            const email= form.value.email;
+            const password = form.value.password;
+            this.isLoadingMode = true;
+
+            if(this.isLoginMode){
+                authObs= this.authService.login(email,password)
+            }
+            else
+            {
+                authObs= this.authService.signup(email,password)
+
+            }
+            authObs.subscribe(
+
+                resData =>{
+                    console.log(resData);
+                    this.isLoadingMode=false;
+                    this.router.navigate(['/recipes']);
+                },
+
+                errorMessage => {
+                    this.error = errorMessage; 
+                    this.isLoadingMode = false;
+                }
+            );
+            form.reset();
+        }
+       
+    }
+
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit,OnDestroy{
 
-  constructor(private dataStorageService:DataStorageService){}
+  private userSub: Subscription;
+  isAuthenticated = false;
 
+  constructor(private dataStorageService:DataStorageService,
+              private authService:AuthService,private router:Router){}
+
+  ngOnInit(): void {
+
+    this.userSub = this.authService.user.subscribe(
+      user=> {
+
+        this.isAuthenticated = !user ? false : true; 
+
+      }
+    )
+      
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe;
+  }
   onSaveDate(){
 
     this.dataStorageService.storeRecipe()
@@ -18,5 +40,8 @@ export class HeaderComponent {
     this.dataStorageService.fetchRecipe().subscribe()
 
   }
- 
+  onLogout(){
+     this.authService.logout();
+
+  }
 }
